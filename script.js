6b81e47716a74277e9e66588159e8dfe48b40c6a
Maria Júlia <mjuliamary0@gmail.com>
2024-09-11 18:58:33 +0000

    requisito cadastrar tarefa

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const cadastrarTarefa = (event) => {
   event.preventDefault();
-  const tarefa = document.querySelector('input[name="tarefa"]').value;
+
+  const tarefa = document.querySelector('input').value;
+  if (tarefa.trim() === "") {
+    alert("Por favor, insira uma tarefa.");
+    return;
+  }
+
   const lista = document.querySelector('ul');
   const li = document.createElement('li');
   li.textContent = tarefa;
   lista.appendChild(li);
+
+  document.querySelector('input').value = ''; 
 }
